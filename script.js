d429c13bd4b086f2d23e9c051914f3561b597f83
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-24 15:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function rotateLine() {
+    const line = document.getElementById('line');
+    let angle = 0;
+
+    function rotate() {
+        angle = (angle + 2) % 360; // Increase angle by 2 degrees, wrapping around at 360 degrees
+        line.style.transform = `rotate(${angle}deg)`;
+        requestAnimationFrame(rotate); // Request next frame
+    }
+
+    rotate(); // Start the rotation
+}
+
+window.onload = rotateLine;
